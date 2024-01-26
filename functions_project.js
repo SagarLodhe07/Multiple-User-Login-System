b2a499e96d88b91ce9afa78f06e784b63dd88616
Sagar Lodhe <156011005+SagarLodhe07@users.noreply.github.com>
2024-01-26 19:07:42 +0530

    Add files via upload

---
+++ functions_project.js
@@ -0,0 +1,33 @@
+const loginDetails = [
+  { email: "Sagar@pw", password: "S12435pw" },
+  {
+    email: "Yogesh@pw",
+    password: "Y252435pw",
+  },
+  {
+    email: "Harsh@pw",
+    password: "S1035pw",
+  },
+];
+
+function login_page(userEmail, userPassword) {
+  //Condition 1 Email And Password Both Are match
+  const conditionmatch = loginDetails.find(
+    (u) => u.email === userEmail && u.password === userPassword
+  );
+
+  //Condition 2 Email matched but Password doesn't match
+  const conditionNotmatchWithPasswprd = loginDetails.find(
+    (u) => u.email === userEmail && u.password !== userPassword
+  );
+
+  if (conditionmatch) {
+    return `Login Successfull`;
+  } else if (conditionNotmatchWithPasswprd) {
+    return `Password Not match`;
+  } else {
+    return `login failed`;
+  }
+}
+
+console.log(login_page("Sagar@pw", "S12435pw"));
